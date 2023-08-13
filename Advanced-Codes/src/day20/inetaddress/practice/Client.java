6566c7085c7719b3f0b332b54dfcc0f699b4dbc8
ZJ920 <1622781044@qq.com>
2023-08-13 19:00:04 +0800

    网络编程-大作业（写不出来了）

--- Advanced-Codes/src/day20/inetaddress/practice/Client.java
+++ Advanced-Codes/src/day20/inetaddress/practice/Client.java
@@ -1,7 +1,6 @@
 package day20.inetaddress.practice;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
@@ -29,12 +28,17 @@ public class Client {
             String str = scanner.nextLine();
             if ("1".equals(str)) {
                 //登录
-                login(scanner, os);
+                registerAndLogin("1.",scanner, os, socket);
                 break;
             } else if ("2".equals(str)) {
                 //注册
-                register(scanner, os,socket);
-                break;
+                boolean temp = registerAndLogin("2.",scanner, os,socket);
+                if (temp){
+                    System.out.println("注册成功，请登录");
+                    registerAndLogin("1.",scanner, os, socket);
+                }else {
+                    System.out.println("注册失败");
+                }
             } else {
                 System.out.println("输入有误，请重新输入:1.登录;2.注册");
                 System.out.println("--------------------------------------------");
@@ -53,24 +57,8 @@ public class Client {
         socket.close();
     }
 
-    //登录
-    public static void login(Scanner scanner, OutputStream os) throws IOException {
-        while (true) {
-            System.out.println("--------------------------------------------");
-            System.out.println("请输入用户名：");
-            String userName = scanner.nextLine();
-            System.out.println("请输入密码：");
-            String password = scanner.nextLine();
-            //选项标识+用户名+密码
-            String data = "1." + "userName=" + userName + "&" + "password=" + password;
-            os.write(data.getBytes(StandardCharsets.UTF_8));
 
-            break;
-        }
-    }
-
-    //注册
-    public static void register(Scanner scanner, OutputStream os,Socket socket) throws IOException {
+/*    public static void login(Scanner scanner, OutputStream os, Socket socket) throws IOException {
         while (true) {
             System.out.println("--------------------------------------------");
             System.out.println("请输入用户名：");
@@ -86,9 +74,56 @@ public class Client {
                 System.out.println("用户名过长或密码格式错误，请重新输入");
             } else {
                 //选项标识+用户名+密码
-                String data = "2." + "userName=" + userName + "&" + "password=" + password;
+                String data = "1." + "userName=" + userName + "&" + "password=" + password;
                 os.write(data.getBytes());
+
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                int a;
+                while ((a = isr.read()) != -1) {
+                    System.out.print((char) a);
+                }
+                is.close();
+                //socket.close();
                 break;
+            }
+        }
+    }
+*/
+    //注册、登录
+    public static boolean registerAndLogin(String index,Scanner scanner, OutputStream os,Socket socket) throws IOException {
+        while (true) {
+            System.out.println("--------------------------------------------");
+            System.out.println("请输入用户名：");
+            String userName = scanner.nextLine();
+            System.out.println("请输入密码：");
+            String password = scanner.nextLine();
+            //只能由字母、数字组成1-10位
+            String regex = "^[a-zA-Z0-9]{1,10}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(password);
+
+            if (userName.length() > 10 || !matcher.matches()) {
+                System.out.println("用户名过长或密码格式错误，请重新输入");
+            } else {
+                //选项标识+用户名+密码
+                String data = index + "userName=" + userName + "&" + "password=" + password;
+                os.write(data.getBytes());
+
+                socket.shutdownOutput();
+
+                InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+                BufferedReader br = new BufferedReader(isr);
+                String a;
+                while ((a = br.readLine()) != null) {
+                    if ("注册成功".equals(a)){
+                        return true;
+                    }
+                }
+                br.close();
+                //socket.close();
+
+                return false;
             }
         }
     }
--- Advanced-Codes/src/day20/inetaddress/practice/MyThread.java
+++ Advanced-Codes/src/day20/inetaddress/practice/MyThread.java
@@ -43,30 +43,57 @@ public class MyThread implements Runnable {
             String userdata = userName + "=" + password;
             //System.out.println(index);
             //System.out.println(index+userName+password);
-            if ("1".equals(index)){
+            if ("1".equals(index)) {
+                BufferedReader br = new BufferedReader(new FileReader("Advanced-Codes\\src\\day20\\inetaddress\\practice\\User.txt"));
 
-            }
-            //注册
-            if ("2".equals(index)){
-                //锁:防止多线程同时写入导致数据错误
-                try {
-                    lock.lock();
-                    OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream("Advanced-Codes\\src\\day20\\inetaddress\\practice\\User.txt",true));
-                    BufferedWriter bw = new BufferedWriter(osw);
-                    bw.write(userdata);
-                    bw.newLine();
-                    bw.flush();
-                    bw.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }finally {
-                    lock.unlock();
+                String str;
+                while ((str = br.readLine()) != null) {
+                    if (userdata.equals(str)) {
+                        System.out.println("登录");
+                    } else {
+                        System.out.println("失败");
+                    }
                 }
 
                 OutputStream os = socket.getOutputStream();
                 os.write("已接收".getBytes());
                 os.close();
+            }
+            //注册
+            if ("2".equals(index)) {
 
+                BufferedReader br = new BufferedReader(new FileReader("Advanced-Codes\\src\\day20\\inetaddress\\practice\\User.txt"));
+
+                String str;
+                while ((str = br.readLine()) != null) {
+                    //张三=123456
+                    str = str.split("=")[0];
+                    if (userName.equals(str)) {
+                        System.out.println("该用户已存在");
+
+                        OutputStream os = socket.getOutputStream();
+                        os.write("注册失败".getBytes());
+                        os.close();
+                    } else {
+                        //锁:防止多线程同时写入导致数据错误
+                        try {
+                            lock.lock();
+                            OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream("Advanced-Codes\\src\\day20\\inetaddress\\practice\\User.txt", true));
+                            BufferedWriter bw = new BufferedWriter(osw);
+                            bw.write(userdata);
+                            bw.newLine();
+                            bw.flush();
+                            bw.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            lock.unlock();
+                        }
+                        OutputStream os = socket.getOutputStream();
+                        os.write("注册成功".getBytes());
+                        os.close();
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
